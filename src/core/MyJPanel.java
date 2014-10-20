87702d8639636c59cc44565ec0311dfb244cfd6a
LLFRealms <llfrealms@gmail.com>
2014-10-20 04:46:58 -0400

    added rollCredits JButton, this is to temporarily show the credits are working

--- src/core/MyJPanel.java
+++ src/core/MyJPanel.java
@@ -4,6 +4,9 @@ Changelog
 19 October 2014
     -1600 hours
         *initial creation
+20 October 2014
+    -0400
+        *added rollCredits JButton, this is to temporarily show the credits are working
 */
 
 package core;
@@ -16,16 +19,20 @@ import javax.swing.*;
  * @author Jackson
  * @version 1.00
  */
-public class MyJPanel extends JPanel 
+public class MyJPanel extends JPanel
 {
     /**
      * Constructor for the class
      * @since version 1.00
      */
+    JButton rollCredits;
+    
     public MyJPanel()
     {
         super();
         setBackground(Color.gray);
     }
+
+
 	
 }
