5ba8bc2e78d994e290238bacfe45cd469010995a
LLFRealms <llfrealms@gmail.com>
2014-10-19 20:39:54 -0400

    Compile credits method created and array list to hold compiled credits created

--- src/credits/Credits.java
+++ src/credits/Credits.java
@@ -9,6 +9,7 @@ Changelog
 */
 package credits;
 
+import java.util.ArrayList;
 import util.XML_240;
 
 /**
@@ -21,15 +22,18 @@ public class Credits {
     /**
      * Creates multiple contributors based on an XML document.
      */
+    Contributor jack, michelle, nick;
+    ArrayList<String> compiledCredits;
     public Credits()
     {
         /* ====================================================
          * This section creates the new instances of the contributors and the XML_240 class
          * ====================================================
          */
-        Contributor jack = new Contributor();
-        Contributor michelle = new Contributor();
-        Contributor nick = new Contributor();   
+        compiledCredits = new ArrayList();
+        jack = new Contributor();
+        michelle = new Contributor();
+        nick = new Contributor();   
         XML_240 x2 = new XML_240();
         
         /* ====================================================
@@ -41,6 +45,29 @@ public class Credits {
         michelle = (Contributor)x2.ReadObject();
         nick = (Contributor)x2.ReadObject();
         x2.closeReaderXML();
+    }
+    
+    public void compileCredits()
+    {
+        compiledCredits.add("Credits");
+        compiledCredits.add(jack.getName());
+        compiledCredits.add("Contributions:");
+        for(int i =0; i < jack.getContribution().size(); i++)
+        {
+            compiledCredits.add(jack.getContribution(i));
+        }
+        compiledCredits.add(michelle.getName());
+        compiledCredits.add("Contributions:");
+        for(int i =0; i < michelle.getContribution().size(); i++)
+        {
+            compiledCredits.add(michelle.getContribution(i));
+        }
+        compiledCredits.add(nick.getName());
+        compiledCredits.add("Contributions:");
+        for(int i =0; i < nick.getContribution().size(); i++)
+        {
+            compiledCredits.add(nick.getContribution(i));
+        }
     }
     
     
