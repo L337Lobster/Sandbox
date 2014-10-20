136f419a5c7a27c3d56d1e9b8df43c697dec493e
LLFRealms <llfrealms@gmail.com>
2014-10-20 04:46:43 -0400

    added rollCredits JButton, this is to temporarily show the credits are working

--- src/core/MyJFrame.java
+++ src/core/MyJFrame.java
@@ -4,6 +4,9 @@ Changelog
 19 October 2014
     -1600 hours
         *initial creation
+20 October 2014
+    -0400
+        *added rollCredits JButton, this is to temporarily show the credits are working
 */
 
 package core;
@@ -11,6 +14,8 @@ package core;
 import credits.Credits;
 import credits.CreditsPanel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
@@ -19,21 +24,36 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
  * @author Jackson
  * @version 1.00
  */
-public class MyJFrame extends JFrame
+public class MyJFrame extends JFrame implements ActionListener
 {
+    MyJPanel mjp;
+    CreditsPanel mcp;
     public MyJFrame ()
     {
         super ("Game");
 
         Credits credits = new Credits();
-        MyJPanel mjp = new MyJPanel();
-        CreditsPanel mcp = new CreditsPanel(credits);
-
+        mjp = new MyJPanel();
+        mcp = new CreditsPanel(credits);
+        mjp.rollCredits = new JButton("Roll Credits");
+        mjp.add(mjp.rollCredits);
+        mjp.rollCredits.addActionListener(this);
         getContentPane().add(mjp,"Center");
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize (640, 480);
         setVisible(true);
     }
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        
+        if(obj == mjp.rollCredits) 
+        { 
+            this.remove(mjp);
+            this.add(mcp);
+            this.revalidate();
+            this.repaint();
+        }
+    }
 }
