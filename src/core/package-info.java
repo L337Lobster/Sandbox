4c605926fcbd9c1baed41513d7cc09c0c36dfb2f
Jack <Jack@JacksSurface>
2014-11-06 14:58:30 -0500

    asdf

---
+++ src/core/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The core package contains the core components.
+ * This includes, but is not limited to:
+ * <ul>
+ * <li>The main JFrame
+ * <li>The main menu JPanel
+ * <li>The main class that creates the main JFrame
+ * </ul>
+ */
+package core;
