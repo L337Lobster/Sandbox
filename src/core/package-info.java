50bd1dd97774dc09cf1bbe203c4339319d9ba645
L337Lobster <jph5321@psu.edu>
2014-11-06 15:40:37 -0500

    javadoc comments

--- src/core/package-info.java
+++ src/core/package-info.java
@@ -3,7 +3,7 @@
  * This includes, but is not limited to:
  * <ul>
  * <li>The {@link core.MainFrame main JFrame}
- * <li>The {@link core.SplashPanel main menu JPanel}
+ * <li>The {@link core.SplashPanel main-menu JPanel}
  * <li>The {@link core.app main class} that creates the main JFrame
  * </ul>
  */
